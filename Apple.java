9fab5922294a3cadeec6743dc324552f36c66f8a
doMOBSTERnique <348787912@gapps.yrdsb.ca>
2025-11-28 13:55:14 -0500

    changed myWorld and created Apple

---
+++ Apple.java
@@ -0,0 +1,31 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Apple here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Apple extends Actor
+{
+    int speed = 1;
+    
+    public void act()
+    {
+        int x = getX();
+        int y = getY() + speed;
+        setLocation(x, y);
+        
+        MyWorld world = (MyWorld) getWorld();
+        if(getY() >= world.getHeight())
+        {
+            world.gameOver();
+            world.removeObject(this);
+        }
+    }
+    
+    public void setSpeed(int spd)
+    {
+        speed = spd;
+    }
+}
--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,45 @@
 import greenfoot.*;
 
 public class MyWorld extends World {
+    public int score = 0;
+    Label scoreLabel;
+    int level = 1;
+    
     public MyWorld() {
-        super(600, 400, 1);
+        super(600, 400, 1, false);
+        
+        Elephant elephant = new Elephant();
+        addObject(elephant, 300, 300);
+        
+        scoreLabel = new Label(0, 80);
+        addObject(scoreLabel, 50, 50);
+        
+        spawnApple();
+    }
+    
+    public void gameOver()
+    {
+        Label gameOver = new Label("Game Over",100);
+        addObject(gameOver, 300, 200);
+    }
+    
+    public void increaseScore()
+    {
+        score++;
+        scoreLabel.setValue(score);
+        
+        if(score % 5 == 0)
+        {
+            level+=1;
+        }
+    }
+    
+    public void spawnApple()
+    {
+        Apple apple = new Apple();
+        apple.setSpeed(level);
+        int x = Greenfoot.getRandomNumber(600);
+        int y = 0;
+        addObject(apple, x, y);
     }
 }
