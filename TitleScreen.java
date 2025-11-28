fbb8c50f00dbb4279708b329cc6187428c331570
doMOBSTERnique <348787912@gapps.yrdsb.ca>
2025-11-28 14:49:00 -0500

    Title Screen

--- TitleScreen.java
+++ TitleScreen.java
@@ -19,6 +19,7 @@ public class TitleScreen extends World
         super(600, 400, 1); 
 
         addObject(titleLabel, getWidth() / 2, 200);
+        prepare();
     }
 
     // The main world act loop
@@ -30,5 +31,26 @@ public class TitleScreen extends World
             MyWorld gameWorld = new MyWorld();
             Greenfoot.setWorld(gameWorld);
         }
+    }
+    
+    private void prepare()
+    {
+        Elephant elephant = new Elephant();
+        addObject(elephant,366,87);
+        elephant.setLocation(392,112);
+        elephant.setLocation(338,99);
+        elephant.setLocation(422,113);
+        Label label = new Label("Press <space> to Start", 40);
+        addObject(label,178,252);
+        label.setLocation(231,335);
+        Label label2 = new Label("Use \u2190 and \u2192 to Move", 40);
+        addObject(label2,200,239);
+        elephant.setLocation(508,137);
+
+        label2.setLocation(308,234);
+
+        label.setLocation(313,302);
+
+        elephant.setLocation(463,121);
     }
 }
