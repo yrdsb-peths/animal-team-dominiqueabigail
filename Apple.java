5eef4047e8938bf8eb2beb5773576e8ac56f038d
doMOBSTERnique <348787912@gapps.yrdsb.ca>
2025-11-28 14:23:04 -0500

    changed Apple to Mosquito and resized image

--- Apple.java
+++ Apple.java
@@ -10,6 +10,15 @@ public class Apple extends Actor
 {
     int speed = 1;
     
+    public Apple()
+    {
+        setImage("mosquitobackpain.png");
+        
+        GreenfootImage mosquito = getImage();
+        mosquito.scale(50,50);
+        setImage(mosquito);
+    }
+    
     public void act()
     {
         int x = getX();
